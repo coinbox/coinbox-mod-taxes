533226afd5107f69cc1f2d5e8d9ea066d939c7ef
jadkik <jadkik94@gmail.com>
2013-05-09 00:58:44 +0300

    Dummy tax implementation. For testing purposes.

--- cbpos/mod/taxes/__init__.py
+++ cbpos/mod/taxes/__init__.py
@@ -1,7 +1,28 @@
+from pydispatch import dispatcher
+
 import cbpos
 from cbpos.modules import BaseModuleLoader
+
+logger = cbpos.get_logger(__name__)
 
 class ModuleLoader(BaseModuleLoader):
     dependencies = ('base', 'currency', 'sales')
     config = [['mod.taxes', {}]]
     name = 'Taxes Support'
+
+    def init(self):
+        dispatcher.connect(self.onTaxesUpdate, signal='update-taxes')
+        
+        return True
+    
+    def onTaxesUpdate(self, signal, sender, manager):
+        """
+        Dummy implementation of taxes
+        """
+        logger.debug('Signal received is {} sent by {}'.format(signal, sender))
+        
+        logger.debug('Ticket is: {}'.format(manager.ticket))
+        
+        if manager.ticket is not None:
+            for tl in manager.ticket.ticketlines:
+                tl.taxes = 0.1*tl.sell_price*tl.amount
